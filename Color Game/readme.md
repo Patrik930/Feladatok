92dcbc3b159ba7602dd29da610fade7c58453359
Your Name <you@example.com>
2023-01-25 14:25:33 +0100

    CSS3 Különleges Témakörök

---
+++ Color Game/readme.md
@@ -0,0 +1,30 @@
+
+# Verziókezelés 
+
+Parancssorban PAT-al:
+
+- Helyi repository inicializálása:
+    - **git init**
+
+- Projekt könyvtár ellenőrzése:
+    - **git status**
+
+- Ha van elmenteni való akkor előkészítjük a színpadra(stage):
+    - **git add .**
+
+- Ellenőrzés:
+    - **git status**
+
+- Elementjük a helyi repoba(commitoljuk):
+    - **git commit -m "First Commit"**
+
+- Ellenőrzés:
+    - **git status**
+
+- létrehozunk a GitHub-on egy repot
+- PAT használva összekapcsoljuk a   GitHub-on létrehozott repoval
+
+- feltesszük a távoli repoba(ez csak első pushkor kell utána csak git push)
+    - **git push -u origin master**
+
+[Color Game](https://patrik930.github.io/ColorGame/)
--- Color Game/script.js
+++ Color Game/script.js
@@ -1,25 +1,28 @@
-function makeColor(){
-    let r=Math.floor(Math.random()*256)
-    let g=Math.floor(Math.random()*256)
-    let b=Math.floor(Math.random()*256)
+function makeColor() {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
     return `rgb(${r}, ${g}, ${b})`
 }
 
 //console.log(makeColor())
+let match = false
+let colors = []
+let squareArr = document.querySelectorAll('.square')
 
-let colors=[]
-let squareArr=document.querySelectorAll('.square')
-
-function init(){
+function init() {
     //scp-096
-    for(let obj of squareArr){
+    colors = []
+    for (let obj of squareArr) {
         //console.log(obj)
-        let color=makeColor()
-        obj.style.backgroundColor=color
+        let color = makeColor()
+        obj.style.backgroundColor = color
         colors.push(color)
     }
-    let index = Math.floor(Math.random()*6)
-    document.getElementById('color').innerHTML=colors[index]
+    let index = Math.floor(Math.random() * 6)
+    document.getElementById('color').innerHTML = colors[index]
+    document.querySelector('i').innerHTML = ''
+    match = false
 
 }
 
@@ -29,20 +32,24 @@ init()
 console.log(colors)
 
 
-function verify(e){
-    console.log(e.target.style.backgroundColor)
-    let clickedColor = e.target.style.backgroundColor
-    let c=document.getElementById('color').innerHTML 
-    if(clickedColor == c){
-        document.querySelector('h1').style.backgroundColor = clickedColor
-        document.querySelector('i').innerHTML = '😀'
-    }else{
-        document.querySelector('i').innerHTML = ''
+function verify(e) {
+    if (!match) {
+        console.log(e.target.style.backgroundColor)
+        let clickedColor = e.target.style.backgroundColor
+        let c = document.getElementById('color').innerHTML
+        if (clickedColor == c) {
+            document.querySelector('h1').style.backgroundColor = clickedColor
+            document.querySelector('i').innerHTML = '😀'
+            match = true
+        } else {
+            document.querySelector('i').innerHTML = '☹️'
+        }
     }
+
 }
 
 
-function uj(){
+function uj() {
     document.querySelector('h1').style.backgroundColor = ''
-    init()    
+    init()
 }
